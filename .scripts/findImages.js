88accbc2e36de86657ed74028c1aefc683b87da9
tianke.lyq <tianke.lyq@alibaba-inc.com>
2019-05-01 16:34:47 +0800

    update

--- .scripts/findImages.js
+++ .scripts/findImages.js
@@ -18,29 +18,27 @@ function getFiles(dir, files_) {
 
 const files = getFiles(path.join(__dirname, '../themes'));
 const urls = files.reduce((last, file) => {
-  const curUrls = fs.readFileSync(file, 'utf-8').toString().match(/http(.+).sinaimg.cn(.+)\)/g);
+  const curUrls = fs.readFileSync(file, 'utf-8').toString().match(/http(.+).sinaimg.cn(.+)(jpg|gif)/g);
   if (!curUrls) { return last; }
   return [...last, ...curUrls];
-}, []).map(url => url.replace(/\)$/, ''));
+}, []);
 
 const map = {};
 urls.forEach((url, i) => {
-  // const ext = url.match(/(gif|jpg|png)/g)[0];
-  const fileName = new Date().getTime() + '-' + i + '.jpg';
+  const ext = url.match(/(gif|jpg|png)/g) ? url.match(/(gif|jpg|png)/g)[0]: 'jpg';
+  const fileName = new Date().getTime() + '-' + i + '.' + ext;
   download(url.replace('https', 'http'), 'themes/landscape/source/css/images/' + fileName , console.log(url, '下载完成'));
   map[url] = fileName;
 });
 
 files.forEach((file) => {
-  if (file.match(/\.md$/)) {
-    for (url in map) {
-      const content = fs.readFileSync(file, 'utf-8').toString();
-      if (content.includes(url)) {
-        console.log('在', file, '找到图片 URL，正在处理……')
-        const newContent = content.replace(url, '/css/images/' + map[url]);
-        fs.writeFileSync(file, newContent);
-        console.log(file, '处理完成');
-      }
+  for (url in map) {
+    const content = fs.readFileSync(file, 'utf-8').toString();
+    if (content.includes(url)) {
+      console.log('在', file, '找到图片 URL，正在处理……')
+      const newContent = content.replace(url, '/css/images/' + map[url]);
+      fs.writeFileSync(file, newContent);
+      console.log(file, '处理完成');
     }
   }
 });
